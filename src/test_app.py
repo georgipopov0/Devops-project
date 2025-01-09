0868a4c8d1c21ef6df5e161f4df7f2520d5c0a29
georgipopov0 <georgipopov069@gmail.com>
2025-01-09 15:15:24 +0200

    first commit

---
+++ src/test_app.py
@@ -0,0 +1,8 @@
+from app import app
+
+def test_hello():
+    # We use the test client provided by Flask
+    test_client = app.test_client()
+    response = test_client.get("/")
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Hello, DevOps!"}
