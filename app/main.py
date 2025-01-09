a41593695b10da7946b7bc5168739675177951a8
georgipopov0 <georgipopov069@gmail.com>
2025-01-09 15:55:28 +0200

    Fixed checkstyle

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return jsonify({"message": "Hello, DevOps!"})
@@ -9,4 +10,3 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="192.168.0.1", port=5000)
-
