aac1aaa96e301c83c956e71c8b95a771cc78f260
georgipopov0 <georgipopov069@gmail.com>
2025-01-09 15:44:02 +0200

    Fixed stye

--- app/app.py
+++ app/app.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return jsonify({"message": "Hello, DevOps!"})
+
 
 if __name__ == "__main__":
     app.run(host="192.168.0.1", port=5000)
