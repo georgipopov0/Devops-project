0629326e8e8f3997525f283467a8ab95a5173e13
georgipopov0 <georgipopov069@gmail.com>
2025-01-09 15:52:59 +0200

    renamed main file

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello():
     return jsonify({"message": "Hello, DevOps!"})
@@ -10,3 +9,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="192.168.0.1", port=5000)
+
